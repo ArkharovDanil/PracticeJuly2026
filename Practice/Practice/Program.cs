b6c80a1d48f28e1c69ccc7c5a2cfd3252844d62b
ArkharovDanil <maslinaraz@gmail.com>
2026-07-08 21:06:08 +0300

    Revert "draft version of first task"
    This reverts commit 8040fe6eba72a4aaf870f309c7014aff97b9ea3c.

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,6 +1,7 @@
-﻿using Practice.Classes;
-using System;
-using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Practice
@@ -16,26 +17,6 @@ namespace Practice
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Do();
-            Graphics gr = new Graphics();
-
-
-
-        }
-
-        // Draw(Graphics gr)
-
-        static void Do()
-        {
-            Point point1 = new Point();
-            point1.SetX(0);
-            point1.SetY(0);
-            Point point2 = new Point();
-            point2.SetX(1);
-            point2.SetY(1);
-
-            Line k = new Line(point1,point2);
-            k.GetPoint(0.3);//
         }
     }
 }
